6c94bf14f6d55d06ed3826c03c04f49ec4a623c9
Florian <floparreno@gmail.com>
2011-12-24 17:17:24 +0100

    correction bug commit

--- app/controllers/groupes_controller.php
+++ app/controllers/groupes_controller.php
@@ -8,10 +8,6 @@
 		
 		public function index($groupeID = null)
 		{
-<<<<<<< HEAD
-			debug($this->Auth->password('profomglmac'));
-=======
->>>>>>> 2f1e14be995b02ec427ee8d4b51990ab45586ca8
 			$groupeID = (is_null($groupeID)) ? $this->Auth->user('groupe_id') : $groupeID;
 			$this->Groupe->recursive = 0;
 			$d['groupe'] = $this->Groupe->findById($groupeID);
