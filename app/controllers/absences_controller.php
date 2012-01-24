bdc1bc53713099ed0199957462c6aa553652dfc5
a-mt <amistri@live.fr>
2012-01-24 15:25:28 +0100

    modif des absences debug

--- app/controllers/absences_controller.php
+++ app/controllers/absences_controller.php
@@ -6,7 +6,6 @@ class AbsencesController extends AppController {
 	var $uses = array('Absence', 'Personne');
 
 	function index($id = null) {
-
 		if (isset($this->data))
 		{
 			$this->Absence->set($this->data);
@@ -21,8 +20,13 @@ class AbsencesController extends AppController {
 				$this->Session->setFlash('L\'absence est incorrecte', 'message');
 			}
 		}
-		elseif (!is_null($id))
+		elseif (!is_null($id)) 
+		{
 			$this->data = $this->Absence->find('first', array('conditions' => array('Absence.id' => $id), 'recursive' => -1));
+			$personne = $this->Personne->find('first', array('conditions' => array('id' => $this->data['Absence']['personne_id'])));
+			$this->data['Absence']['personne_id'] = $personne['Personne']['display'];
+			$this->data['modif'] = "";
+		}
 
 		$pers['personnes'] = $this->Personne->find('list', array('conditions' => array('statut_id' => $this->statuts['eleve'])));
 		$pers['absences'] = $this->Absence->find('all');
